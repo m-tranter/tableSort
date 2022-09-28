1f8ae465a8088a1a121aed97d1d4b1a7ed779943
antiloquax <tranter.m@sky.com>
2022-09-28 21:30:32 +0100

    experimenting - needs refactoring

--- app.js
+++ app.js
@@ -1,106 +1,53 @@
 'use strict';
-const myTable = document.getElementById('myTable');
-var items = [];
-var heads = [];
-var types = [];
-var lastSort = '';
-var op = 1;
+const reg = /^-?[0-9]\d*(\.\d+)?/;
 
-(function getTableData() {
-    for (var i = 0; i < myTable.rows.length; i++) {
-        if (i === 0) {
-            for (var cell of myTable.rows[i].cells) {
-                heads.push(cell.innerHTML);
-                setUp(cell);
-            }
-        } else {
-            items.push({});
-            for (var j = 0; j < myTable.rows[i].cells.length; j++) {
-                items[i - 1][heads[j]] = myTable.rows[i].cells[j].innerHTML;
-            }
-        }
-        if (i === 1) {
-            for (var k = 0; k < myTable.rows[i].cells.length; k++) {
-                let f = heads[k];
-                let date = parseDate(myTable.rows[i].cells[k].innerHTML);
-                if (date != null) {
-                    types.push(sortDate(f));
-                } else if (
-                    isNaN(parseInt(myTable.rows[i].cells[k].innerHTML))
-                ) {
-                    types.push(sortStr(f));
-                } else if (isNaN(parseInt(myTable.rows[i].cells[k].innerHTML)) &&
-                    !isNaN(parseInt(myTable.rows[i].cells[k].innerHTML[0]))
-                ) {
-                    types.push(sortInitialNum(f));
-                } else {
-                    types.push(sortNum(f));
-                }
-            }
-        }
-    }
-})();
-
-function getDigits(a) {
-    let temp = a.split(" ")[0];
-    let num = parseInt(temp);
-    return (isNaN(num)) ? parseInt(temp.slice(0, -1)) : num;
+for (var table of document.getElementsByTagName('table')) {
+  sortableTable(table);  
 }
 
-function sortInitialNum(f) {
+
+function sortableTable(myTable) {
+
+  function sortInitialNum(f) {
     return (a, b) => {
-        return (this.getDigits(a[f]) - this.getDigits(b[f])) * this.op;
+      return (toFloat(a[f]) - toFloat(b[f])) * op;
     };
-}
-
-function redrawTable() {
-     for (var i = 1; i < myTable.rows.length; i++) {
-         for (var j = 0; j < myTable.rows[i].cells.length; j++) {
-             myTable.rows[i].cells[j].innerHTML = items[i - 1][heads[j]];
-         }
-     }
-}
-
-function parseDate(str) {
-  var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
-  return (m) ? new Date(m[3], m[2]-1, m[1]) : null;
-}
-
-function sortNum(f) {
+  }
+  function sortNum(f) {
     return (a, b) => {
-        return (parseInt(a[f]) - parseInt(b[f])) * op;
+      return (parseInt(a[f]) - parseInt(b[f])) * op;
     };
-}
+  }
 
-function sortStr(f) {
-      return (a, b) => {
-        let x = a[f].toLowerCase();
-        let y = b[f].toLowerCase();
-        if (x < y) {
-          return -1 * op;
-        }
-        if (x > y) {
-          return 1 * op;
-        }
-        return 0;
-      };
-    }
+  function sortStr(f) {
+    return (a, b) => {
+      let x = a[f].toLowerCase();
+      let y = b[f].toLowerCase();
+      if (x < y) {
+        return -1 * op;
+      }
+      if (x > y) {
+        return 1 * op;
+      }
+      return 0;
+    };
+  }
 
-function sortDate(f) {
-          return (a, b) => {
-            let x = parseDate(a[f]); 
-            let y = parseDate(b[f]);
-            if (x < y) {
-              return -1 * op;
-            }
-            if (x > y) {
-              return 1 * op;
-            }
-            return 0;
-          };
-        }
+  function sortDate(f) {
+    return (a, b) => {
+      let x = parseDate(a[f]); 
+      let y = parseDate(b[f]);
+      if (x < y) {
+        return -1 * op;
+      }
+      if (x > y) {
+        return 1 * op;
+      }
+      return 0;
+    };
+  }
 
-function setUp(e) {
+  function setUp(e) {
     let temp = e.innerHTML;
     e.setAttribute("tabindex", "0");
     e.addEventListener("mouseover", () => e.style.backgroundColor = "PaleGreen");
@@ -115,12 +62,67 @@ function setUp(e) {
     });
   }
 
-function sortByField(f) {
+  function sortByField(f) {
     op = (lastSort === f) ? -op : 1;
     lastSort = f;
     items.sort(types[heads.indexOf(f)]);
-    redrawTable();
+    redrawTable(myTable);
+  }
+  function redrawTable(myTable) {
+    for (var i = 1; i < myTable.rows.length; i++) {
+      for (var j = 0; j < myTable.rows[i].cells.length; j++) {
+        myTable.rows[i].cells[j].innerHTML = items[i - 1][heads[j]];
+      }
+    }
+  }
+  var items = [];
+  var heads = [];
+  var types = [];
+  var lastSort = '';
+  var op = 0;
+  for (var i = 0; i < myTable.rows.length; i++) {
+    if (i === 0) {
+      for (var cell of myTable.rows[i].cells) {
+        heads.push(cell.innerHTML);
+        setUp(cell);
+      }
+    } else {
+      items.push({});
+      for (var j = 0; j < myTable.rows[i].cells.length; j++) {
+        items[i - 1][heads[j]] = myTable.rows[i].cells[j].innerHTML;
+      }
+    }
+    if (i === 1) {
+      for (var k = 0; k < myTable.rows[i].cells.length; k++) {
+        let f = heads[k];
+        let date = parseDate(myTable.rows[i].cells[k].innerHTML);
+        if (date != null) {
+          types.push(sortDate(f));
+        } else if (
+          isNaN(parseInt(myTable.rows[i].cells[k].innerHTML))
+        ) {
+          types.push(sortStr(f));
+        } else if (myTable.rows[i].cells[k].innerHTML.match(reg) != null) {
+          types.push(sortInitialNum(f));
+        } else {
+          types.push(sortNum(f));
+        }
+      }
+    }
+  }
 }
+
+function toFloat(n) {
+  let match = n.match(reg);
+  return (match === null) ? 0 : parseFloat(match);
+}
+
+function parseDate(str) {
+  var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+  return (m) ? new Date(m[3], m[2]-1, m[1]) : null;
+}
+
+
 
 
 
