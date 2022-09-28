68d85ce9a37448d2868e90697cf8a6ad862ad5c8
antiloquax <tranter.m@sky.com>
2022-09-28 10:34:51 +0100

    Seems to work.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
-
 const myTable = document.getElementById('myTable');
 var items = [];
 var heads = [];
+var types = [];
+var lastSort = '';
 var op = 1;
-
 
 (function getTableData() {
   for (var i = 0; i < myTable.rows.length; i++) {
@@ -13,17 +13,80 @@ var op = 1;
         heads.push(cell.innerHTML);
         setUp(cell);
       }
-    } else {
+    } else {     
       items.push({});
       for (var j = 0; j < myTable.rows[i].cells.length; j++) {
         items[i - 1][heads[j]] = myTable.rows[i].cells[j].innerHTML;
       } 
     }
+      if (i === 1) {
+          for (var k = 0; k < myTable.rows[i].cells.length; k++) {
+              let f = heads[k];
+              let date = parseDate(myTable.rows[i].cells[k].innerHTML);
+              if (date != null) {
+                  types.push(sortDate(f));
+              } else if (isNaN(parseInt(myTable.rows[i].cells[k].innerHTML))) {
+                      types.push(sortStr(f));
+                  } else {
+                      types.push(sortNum(f));
+                  }  
+          }
+      }
   }
 })();
 
+function redrawTable() {
+     for (var i = 1; i < myTable.rows.length; i++) {
+         for (var j = 0; j < myTable.rows[i].cells.length; j++) {
+             myTable.rows[i].cells[j].innerHTML = items[i - 1][heads[j]];
+         }
+     }
+}
+
+function parseDate(str) {
+  var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+  return (m) ? new Date(m[3], m[2]-1, m[1]) : null;
+}
+
+function sortNum(f) {
+    return (a, b) => {
+        console.log("num sort");
+        return (parseInt(a[f]) - parseInt(b[f])) * op;
+    };
+}
+
+function sortStr(f) {
+      return (a, b) => {
+        let x = a[f].toLowerCase();
+        let y = b[f].toLowerCase();
+        if (x < y) {
+          return -1 * op;
+        }
+        if (x > y) {
+          return 1 * op;
+        }
+        return 0;
+      };
+    }
+
+function sortDate(f) {
+          return (a, b) => {
+              console.log("Date sort");
+            let x = parseDate(a[f]); 
+            let y = parseDate(b[f]);
+            if (x < y) {
+              return -1 * op;
+            }
+            if (x > y) {
+              return 1 * op;
+            }
+            return 0;
+          };
+        }
+
 function setUp(e) {
     let temp = e.innerHTML;
+    e.setAttribute("tabindex", "0");
     e.addEventListener("mouseover", () => e.style.backgroundColor = "PaleGreen");
     e.addEventListener("focus", () => e.style.backgroundColor = "PaleGreen");
     e.addEventListener("mouseout", () => e.style.backgroundColor = "White");
@@ -37,85 +100,16 @@ function setUp(e) {
   }
 
 function sortByField(f) {
-  // add sort by field function.
-  console.log(`Sorting by: ${f}`);
-}
-
-
-function clearTable() {
-  for (let i = 0; i < items.length; i++) {
-    myTable.deleteRow(1);
-  }
-}
-
-
-/*
-function sortByField(f) {
-    if (this.lastSort === f) {
+    if (lastSort === f) {
         op *= -1;
-    } else if (this.lastSort === '' && f === 'licenceReference'){
-        op = -1;
     } else {
         op = 1;
     }
-    this.lastSort = f;
-    if it's a date 
-        items.sort(this.sortDate(f));
-    } else if (f === "buildingName") {
-        items.sort(sortNum(f));
-    } else {
-        items.sort(sortStr(f));
-    }
+    lastSort = f;
+    items.sort(types[heads.indexOf(f)]);
+    redrawTable();
 }
 
-function getDigits(a) {
-    let temp = a.split(' ')[0];
-    let num = parseInt(temp);
-    if (isNaN(num)) {
-        num = parseInt(temp.slice(0,-1));
-    }
-    if (isNaN(num)) {
-        num = 0;
-    }
-    return num;
-}
-
-function sortNum(f) {
-    return (a, b) => {
-        return (getDigits(a[f]) - getDigits(b[f])) * op;
-    };
-
-}
-function sortStr(field) {
-      return (a, b) => {
-        let x = a[field].toLowerCase();
-        let y = b[field].toLowerCase();
-        if (x < y) {
-          return -1 * this.op;
-        }
-        if (x > y) {
-          return 1 * this.op;
-        }
-        return 0;
-      };
-    }
-
-
-function sortDate(field) {
-          return (a, b) => {
-            let x = new Date(a[field]); 
-            let y = new Date(b[field]);
-            if (x < y) {
-              return -1;
-            }
-            if (x > y) {
-              return 1;
-            }
-            return 0;
-          };
-        }
-
-*/
 
 
 
