a7bbc28d0a6b2e1b925d8d693cec10b63fa83d63
antiloquax <tranter.m@sky.com>
2022-10-15 11:38:38 +0100

    Using luxon for the dates.

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ function makeSortable(e) {
   let op = 0;
 
   const note = document.createElement("p");
-  note.appendChild(document.createTextNode(
-    "Click on a heading to sort by that column."));
+  note.appendChild(
+    document.createTextNode("Click on a heading to sort by that column.")
+  );
   note.classList.add("cec-green");
   e.parentNode.insertBefore(note, e);
 
-  Array.from(rows[0].cells).forEach(cell => {
+  Array.from(rows[0].cells).forEach((cell) => {
     funcs[cell.innerText] = "";
     setUp(cell);
   });
@@ -22,55 +23,54 @@ function makeSortable(e) {
   Array.from(rows[1].cells).forEach((cell, k) => {
     let f = key(k);
     let str = cell.innerText;
-    let date = parseDate(str);
-    if (date !== null) {
+    if (parseDate(str)) {
       funcs[f] = sortDate(f);
-    } else if (str.match(reg) === null) {
-      funcs[f] = sortStr(f);
     } else {
-      funcs[f] = sortInitialNum(f);
+      funcs[f] = str.match(reg) === null ? sortStr(f) : sortInitialNum(f);
     }
   });
 
-  Array.from(rows).slice(1).forEach((row, i) => {
-    items.push({});
-    Array.from(row.cells).forEach((cell, j) => {
-      items[i][key(j)] = cell.innerHTML;
-      items[i][`${key(j)}inner`] = cell.innerText;
+  Array.from(rows)
+    .slice(1)
+    .forEach((row, i) => {
+      items.push({});
+      Array.from(row.cells).forEach((cell, j) => {
+        items[i][key(j)] = cell.innerHTML;
+        items[i][`${key(j)}inner`] = parseDate(cell.innerText)
+          ? new luxon.DateTime.fromFormat(cell.innerText, "dd/MM/yyyy")
+          : cell.innerText;
+      });
     });
-  });
 
   function key(ind) {
     return Object.keys(funcs)[ind];
   }
 
   function sortInitialNum(f) {
-    f += 'inner';
+    f += "inner";
     return (a, b) => {
       return (toFloat(a[f]) - toFloat(b[f])) * op;
     };
   }
 
-  function sortStr(f) {
-    f += 'inner';
+  function sortDate(f) {
+    f += "inner";
     return (a, b) => {
-      let x = a[f].toLowerCase();
-      let y = b[f].toLowerCase();
-      if (x < y) {
+      if (a[f] < b[f]) {
         return -1 * op;
       }
-      if (x > y) {
+      if (a[f] > b[f]) {
         return 1 * op;
       }
       return 0;
     };
   }
 
-  function sortDate(f) {
-    f += 'inner';
+  function sortStr(f) {
+    f += "inner";
     return (a, b) => {
-      let x = parseDate(a[f]);
-      let y = parseDate(b[f]);
+      let x = a[f].toLowerCase();
+      let y = b[f].toLowerCase();
       if (x < y) {
         return -1 * op;
       }
@@ -88,14 +88,8 @@ function makeSortable(e) {
       "mouseover",
       () => (e.style.backgroundColor = "PaleGreen")
     );
-    e.addEventListener(
-      "focus",
-      () => (e.style.backgroundColor = "PaleGreen")
-    );
-    e.addEventListener(
-      "mouseout",
-      () => (e.style.backgroundColor = "White")
-    );
+    e.addEventListener("focus", () => (e.style.backgroundColor = "PaleGreen"));
+    e.addEventListener("mouseout", () => (e.style.backgroundColor = "White"));
     e.addEventListener("blur", () => (e.style.backgroundColor = "White"));
     e.addEventListener("click", () => sortByField(temp));
     e.addEventListener("keydown", (e) => {
@@ -118,21 +112,20 @@ function makeSortable(e) {
   }
 
   function parseDate(str) {
-    var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
-    return m ? new Date(m[3], m[2] - 1, m[1]) : null;
+    return str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/) !== null;
   }
 
   function redrawTable() {
-    Array.from(rows).slice(1).forEach((row, i) => {
-      Array.from(row.cells).forEach((cell, j) => {
-        cell.innerHTML = items[i][key(j)];
+    Array.from(rows)
+      .slice(1)
+      .forEach((row, i) => {
+        Array.from(row.cells).forEach((cell, j) => {
+          cell.innerHTML = items[i][key(j)];
+        });
       });
-    });
   }
 }
 
-Array.from(document.getElementsByTagName("table")).forEach(e => {
+Array.from(document.getElementsByTagName("table")).forEach((e) => {
   makeSortable(e);
 });
-
-
